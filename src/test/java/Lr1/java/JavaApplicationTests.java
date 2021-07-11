d7cc64e507c5a119ca5f5028b7094f62dec632c1
Gergram <mr.zaluposhka@gmail.com>
2021-07-11 14:08:25 +0300

    ПЗ№1 Hello World

---
+++ src/test/java/Lr1/java/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package Lr1.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
