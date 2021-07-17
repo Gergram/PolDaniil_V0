eb6fbfa5fa7b31c05c97e6d58d3e7fd6b2f577e6
Gergram <mr.zaluposhka@gmail.com>
2021-07-17 16:05:37 +0300

    Задание 2. Реализация сущностей и DAO слоя final

--- src/main/java/Main/java/OrderProduct.java
+++ src/main/java/Main/java/OrderProduct.java
@@ -1,3 +1,5 @@
+package Main.java;
+
 import javax.persistence.*;
 
 @Entity
--- src/test/java/Main/java/TestClassCategory.java
+++ src/test/java/Main/java/TestClassCategory.java
@@ -1,3 +1,5 @@
+package Main.java;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
